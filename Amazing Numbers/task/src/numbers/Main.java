3581d997144c2570e2419563d1c16898f7996d86
Ahmed <ahhosnyas@gmail.com>
2021-05-27 03:23:49 +0200

    trying in Spy numbers

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,11 +1,13 @@
 package numbers;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to Amazing Numbers!");
+        System.out.println();
         printInstructions();
         runProgram();
     }
@@ -16,38 +18,76 @@ public class Main {
                 "  * the first parameter represents a starting number;\n" +
                 "  * the second parameters show how many consecutive numbers are to be processed;\n" +
                 "- separate the parameters with one space;\n" +
-                "- enter 0 to exit.\n" +
-                "\n");
+                "- enter 0 to exit.\n");
     }
 
     static void runProgram(){
         while (true) {
             //getting input
-            long[] longInputArray = Scanner();
-
-            //one number input
-            if (longInputArray.length == 1) {
-                if (!processRequest(longInputArray[0]))
-                    break;
-            }
-            else if (longInputArray.length > 1){
-                if (!processRequest(longInputArray[0], longInputArray[1]))
-                    break;
-            }
-            else {
-                printInstructions();
+            String[] input = Scanner();
+            if (!chooseRequest(input, input.length)){
+                break;
             }
         }
     }
-    static long[] Scanner() {
-        System.out.println("\"Enter a request: > ");
-        String stringInput = scanner.nextLine();
-        String[] stringInputArray = stringInput.split(" ");
+    static String[] Scanner() {
+        System.out.println("\nEnter a request: ");
+        String stringInputArray = scanner.nextLine();
+        return stringInputArray.split(" ");
+    }
+
+    static boolean chooseRequest(String[] stringInputArray, int stringInputArrayLength){
+        boolean isContinue = true;
         long[] longInputArray = new long[stringInputArray.length];
-        for (int i = 0; i < longInputArray.length; i++){
-            longInputArray[i] = Long.parseLong(stringInputArray[i]);
+        if (stringInputArrayLength < 3) {
+            for (int i = 0; i < stringInputArrayLength; i++) {
+                try {
+                    longInputArray[i] = Long.parseLong(stringInputArray[i]);
+                }
+                catch (NumberFormatException e){
+                    if (i == 0){
+                        System.out.println("The first parameter should be a natural number or zero.");
+                    }
+                    if (i == 1){
+                        System.out.println("The second parameter should be a natural number.");
+                        return isContinue;
+                    }
+                }
+            }
         }
-        return longInputArray;
+        else if (stringInputArrayLength == 3){
+            for (int i = 0; i < stringInputArrayLength - 1; i++) {
+                try {
+                    longInputArray[i] = Long.parseLong(stringInputArray[i]);
+                }
+                catch (NumberFormatException e){
+                    if (i == 0){
+                        System.out.println("The first parameter should be a natural number or zero.");
+                    }
+                    if (i == 1){
+                        System.out.println("The second parameter should be a natural number.");
+                        return isContinue;
+                    }
+                }
+
+            }
+        }
+
+        switch (stringInputArrayLength){
+            case 1:
+                isContinue = processRequest(longInputArray[0]);
+                break;
+            case 2:
+                processRequest(longInputArray[0], longInputArray[1]);
+                break;
+            case 3:
+                processRequest(longInputArray[0], longInputArray[1], stringInputArray[stringInputArrayLength - 1]);
+                break;
+            default:
+                printInstructions();
+                break;
+        }
+        return isContinue;
     }
 
     static boolean processRequest(long input){
@@ -62,100 +102,130 @@ public class Main {
         }
         return isContinue;
     }
-    static boolean processRequest(long input1, long input2){
-        boolean isContinue = true;
+    static void processRequest(long input1, long input2){
+        //boolean isContinue = true;
 
         if (input1 > 0 && input2 > 0) {
             printProperties(input1, input2);
         }
-
-        //first parameter
-//        else if (input1 == 0) {
-//            System.out.println("Goodbye!");
-//            isContinue = false;
-//        }
-//        else if (input1 < 0){
-//            System.out.println("The first parameter should be a natural number or zero.");
-//        }
-
-        //second parameter
-//        else if (input2 == 0){
-//            processRequest(input1);
-//        }
         else if (input2 < 0 || input2 == 0){
             System.out.println("The second parameter should be a natural number.");
         }
 
-        return isContinue;
+        //return isContinue;
+    }
+    static void processRequest(long input1, long input2, String filter){
+        if (input1 > 0 && input2 > 0) {
+            printProperties(Objects.requireNonNull(filter(input1, input2, filter)));
+        }
+        else if (input2 < 0 || input2 == 0){
+            System.out.println("\nThe second parameter should be a natural number.");
+        }
+    }
+
+
+    static long[] filter(long input1, long input2, String filter){
+        long[] longs = new  long[(int) input2];
+        filter = filter.toLowerCase();
+        String s = "";
+        int i = 0;
+        while (i < input2){
+            Number number = new Number(input1);
+            switch (filter) {
+                case "even":
+                    if (number.isEven()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                case "odd":
+                    if (number.isOdd()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                case "buzz":
+                    if (number.isBuzz()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                case "duck":
+                    if (number.isDuck()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                case "palindromic":
+                    if (number.isPalindromic()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                case "gapful":
+                    if (number.isGapful()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                case "spy":
+                    if (number.isSpy()) {
+                        longs[i] = number.value;
+                        i++;
+                    }
+                    break;
+                default:
+                    System.out.println("The property [" + filter.toUpperCase() + "] is wrong Available properties: " +
+                            "[BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]");
+                    return null;
+            }
+            input1++;
+        }
+        return longs;
     }
 
     static void printProperties(long input){
+        Number number = new Number(input);
         System.out.println("Properties of " + input);
-        System.out.println("\t\t" + "even: " + isEven(input));
-        System.out.println("\t\t" + " odd: " + isOdd(input));
-        System.out.println("\t\t" + "buzz: " + isBuzz(input));
-        System.out.println("\t\t" + "duck: " + isDuck(input));
-        System.out.println("\t\t" + "Palindromic : " + isPalindromic(input));
-        System.out.println("\t\t" + "gapful: " + isGapful(input));
+        System.out.println("\t\t" + "even: " + number.isEven());
+        System.out.println("\t\t" + " odd: " + number.isOdd());
+        System.out.println("\t\t" + "buzz: " + number.isBuzz());
+        System.out.println("\t\t" + "duck: " + number.isDuck());
+        System.out.println("\t\t" + "Palindromic : " + number.isPalindromic());
+        System.out.println("\t\t" + "gapful: " + number.isGapful());
+        System.out.println("\t\t" + "spy: " + number.isSpy());
         //System.out.println("\t\t" + ": " + );
     }
     static void printProperties(long input1, long input2){
         System.out.println();
         for (int i = 0; i < input2; i++){
+            Number number = new Number(input1);
             System.out.println("\t\t\t" + input1 + " is "
-                            + (isBuzz(input1) ? "buzz, " : "")
-                            + (isDuck(input1) ? "duck, " : "")
-                            + (isEven(input1) ? "even, " : "")
-                            + (isOdd(input1) ? "odd, " : "")
-                            + (isPalindromic(input1) ? "palindromic" : "")
-                            + (isGapful(input1) ? "gapful, " : "")
+                            + (number.isBuzz() ? "buzz, " : "")
+                            + (number.isDuck() ? "duck, " : "")
+                            + (number.isEven() ? "even, " : "")
+                            + (number.isOdd() ? "odd, " : "")
+                            + (number.isPalindromic() ? "palindromic, " : "")
+                            + (number.isGapful() ? "gapful, " : "")
+                            + (number.isSpy() ? "spy, " : "")
                     );
             input1++;
         }
     }
-
-    static boolean isEven (long userInput){
-        return userInput % 2 == 0;
-    }
-    static boolean isOdd (long userInput){
-        return userInput % 2 != 0;
-    }
-    static boolean isBuzz (long userInput) {
-        if (userInput > 10) {
-            long lastDigit = userInput % 10;
-                return userInput % 7 == 0 || lastDigit == 7;
+    static void printProperties(long[] longs){
+        System.out.println();
+        for (int i = 0; i < longs.length; i++){
+            Number number = new Number(longs[i]);
+            System.out.println("\t\t\t" + longs[i] + " is "
+                    + (number.isBuzz() ? "buzz, " : "")
+                    + (number.isDuck() ? "duck, " : "")
+                    + (number.isEven() ? "even, " : "")
+                    + (number.isOdd() ? "odd, " : "")
+                    + (number.isPalindromic() ? "palindromic, " : "")
+                    + (number.isGapful() ? "gapful, " : "")
+                    + (number.isSpy() ? "spy, " : "")
+            );
+            longs[i]++;
         }
-        else return userInput == 7;
-    }
-    static boolean isDuck(long userInput){
-        String toString = String.valueOf(userInput);
-        boolean isDuck = false;
-        for (int i = 1; i < toString.length(); i++){
-            char ch = toString.charAt(i);
-            if (ch == '0'){
-                isDuck = true;
-                break;
-            }
-        }
-        return isDuck;
-    }
-    static boolean isPalindromic (long userInput){
-        String toString = String.valueOf(userInput);
-        StringBuilder sb2 = new StringBuilder(String.valueOf(userInput)).reverse();
-        String reversedString  = sb2.toString();
-        return toString.equals(reversedString);
-    }
-    static boolean isGapful(long input){
-        String s = String.valueOf(input);
-        boolean isGapful = false;
-        if (s.length() >= 3){
-            char fstDigit = s.charAt(0);
-            char lstDigit = s.charAt(s.length() - 1);
-            String concatenated = fstDigit + "" + lstDigit;
-            long concatNumber = Long.parseLong(concatenated);
-            if (input % concatNumber == 0)
-                isGapful = true;
-        }
-        return isGapful;
     }
 }
